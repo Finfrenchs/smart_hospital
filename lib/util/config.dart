35e78e59adc09561cecf1825cb694b7f8209d03e
M. Kelvin Madrianto F <muhammadkelvinmf@gmail.com>
2022-09-27 19:09:57 +0700

    add argument in the config.dart

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -1,3 +1,7 @@
 class AppConfig {
-  static const String API_ENDPOINT = "http://10.1.10.213/smart-hospital/";
+  ///if use mobile device
+  //static const String API_ENDPOINT = "http://10.1.10.213/smart-hospital/";
+
+  ///if use emulator
+  static const String API_ENDPOINT = "http://10.0.2.2/smart-hospital/";
 }
